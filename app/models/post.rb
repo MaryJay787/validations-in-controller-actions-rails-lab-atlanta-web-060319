c7fff8a1d7bfcdb263aaa78f3e7ae1f36ac5b7f4
MaryJay787 <josephmaryam.mj@gmail.com>
2019-07-01 23:24:28 -0400

    Still Working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: { in: %w(fiction non-fiction),
+  message: "%{value} is not a valid selection" }
+  validates :content, length: { maximum: 100 }
 end
